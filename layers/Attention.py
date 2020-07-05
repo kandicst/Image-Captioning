fixed issue with softmax (i hope)

layers/Attention.py
@@ -13,7 +13,7 @@ class Attention(nn.Module):
         self.features_fc = nn.Linear(encoder_dim, attention_dim)
         self.hidden_fc = nn.Linear(decoder_dim, attention_dim)
         self.combined_fc = nn.Linear(attention_dim, 1)
-        self.softmax = nn.Softmax(dim=2)
+        self.softmax = nn.Softmax(dim=1)
 
         self.to(device)
 
@@ -26,7 +26,16 @@ class Attention(nn.Module):
         h_t = self.hidden_fc(h_t)
 
         e_t = torch.tanh(a + h_t)
-        alpha = self.softmax(self.combined_fc(e_t))
+        # print("SCORE: ", e_t.shape)
+        full = self.combined_fc(e_t)
+        # print(full.shape)
+        alpha = self.softmax(full)
+        # print("ATT: ", alpha.shape)
+        # print(alpha[0])
+        # print(alpha.shape)
+        # print(alpha[0].tolist())
         context_vector = (features * alpha).sum(dim=1)
+        # print("CTX: ", context_vector.shape)
+        # raise Exception()
 
         return context_vector, alpha

layers/Decoder.py
@@ -7,30 +7,30 @@ from layers.Attention import Attention
 
 class Decoder(nn.Module):
     def __init__(self, encoder_dim, decoder_hidden_dim, decoder_dim, attention_dim, device,
-                 embedding_dim=256, vocab_size=10000, dropout_p=0.5):
+                 embedding_dim=256, vocab_size=10000, vocab=None, dropout_p=0.5):
         super().__init__()
 
         self.encoder_dims = encoder_dim
         self.decoder_dims = decoder_dim
         self.attention_dim = attention_dim
+        self.vocab = vocab
         self.vocab_size = vocab_size
         self.decoder_hidden = decoder_hidden_dim
         self.embedding_dim = embedding_dim
         self.device = device
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.attention = Attention(encoder_dim, decoder_dim, attention_dim, device)
-        self.rnn = nn.GRU(input_size=encoder_dim + embedding_dim, hidden_size=decoder_hidden_dim, batch_first=True)                          # num_layers=1, bidirectional=False)
-        # self.rnn = nn.LSTM(input_size=encoder_dim + embedding_dim, hidden_size=decoder_hidden_dim, batch_first=True,
-        #                   num_layers=1, bidirectional=False, dropout=dropout_p)
+        self.attention = Attention(encoder_dim, decoder_hidden_dim, attention_dim, device)
+        ### self.rnn = nn.GRU(input_size=encoder_dim + embedding_dim, hidden_size=decoder_hidden_dim, batch_first=True)                          # num_layers=1, bidirectional=False)
+        self.rnn = nn.LSTM(input_size=encoder_dim + embedding_dim, hidden_size=decoder_hidden_dim)
+
+        # self.fc = nn.Linear(decoder_hidden_dim, vocab_size)
 
         self.fc1 = nn.Linear(decoder_hidden_dim, decoder_hidden_dim)
-        self.fc2 = nn.Linear(decoder_dim, vocab_size)
-
+        self.fc2 = nn.Linear(decoder_hidden_dim, vocab_size)
         self.dropout = nn.Dropout(dropout_p)
 
         self.to(device)
-        # self.init_weights()
 
     def forward(self, x, features, hidden):
         x = x.long().to(self.device)
@@ -43,15 +43,17 @@ class Decoder(nn.Module):
         x = torch.cat([x, context_vector], dim=-1)
 
         # output, state = self.rnn(x, hidden.unsqueeze(0))
-        output, state = self.rnn(x)
-        # output, state, cell = self.rnn(x)
-        # state, cell = h
+        # output, state = self.rnn(x)
+        output, h = self.rnn(x)
+        state, cell = h
         state = state.reshape(state.shape[1:])
 
+        # x = x.reshape((-1, x.shape[2]))
+        # x = self.fc(x)
+
         x = self.fc1(output)
-        # x = self.fc1(x)
         x = x.reshape((-1, x.shape[2]))
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.fc2(x)
 
         return x, state, attention_weights

layers/End2End.py
@@ -14,14 +14,20 @@ class End2End(nn.Module):
 
         self.encoder = Encoder(enc_input, enc_output, device)
         self.decoder = Decoder(enc_output, dec_hidden, dec_output,
-                               attn_dim, device, emb_dim, vocab.top_words)
+                               attn_dim, device, emb_dim, vocab.top_words, vocab)
         self.criterion = criterion
         self.vocab = vocab
 
         self.device = device
         self.to(self.device)
 
-    def forward(self, x, captions, train=True):
+    def forward2(self, x, captions):
+        captions = captions.long().to(self.device)
+        features = self.encoder(x)
+
+        return self.decoder(features, captions, self.criterion)
+
+    def forward(self, x, captions):
         captions = captions.long().to(self.device)
         batch_size, caption_length = captions.shape[:2]
         out = torch.zeros((batch_size, caption_length))
@@ -38,6 +44,7 @@ class End2End(nn.Module):
             target = captions[:, i]
             max_words = torch.max(predictions, 1)[1]
             loss = self.criterion(predictions, target.long())
+            # loss += ((1. - attn_weights.sum(dim=1)) ** 2).mean()
             # print(predictions.shape)
             # print(target.long().shape)
             # print('-----------------')
@@ -47,6 +54,10 @@ class End2End(nn.Module):
             out[:, i] = max_words
 
         return out, total_loss
+
+    def evaluate2(self, x):
+        features = self.encoder(x)
+        return self.decoder.evaluate(features)
 
     def evaluate(self, x, captions=None, tf=False):
 